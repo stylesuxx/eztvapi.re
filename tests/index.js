6fc963ade20c4b2d837f9d2471a0513fbaf3bf48
stylesuxx <stylesuxx@gmail.com>
2015-10-12 23:57:22 +0200

    Adapted wording

--- tests/index.js
+++ tests/index.js
@@ -1,21 +1,11 @@
 var test = require('tape');
 var EZTV = require('../');
 
-test('get page count', function (t) {
+test('get page list', function (t) {
   t.plan(1);
 
   var eztv = new EZTV();
-  eztv.getPageCount().then( function(count) {
-    console.log(count);
-    t.equal(count.status, 'ok', 'Request success');
-    /*
-    t.equal(movies.data.movies[0].title, 'Snatch.', 'Title matches');
-    t.equal(movies.data.movies[0].id, 10, 'ID matches');
-    t.ok(movies.data.movies[0].torrents.length, 'Has at least one torrent');
-    t.ok(movies.data.movies[0].background_image, 'Has background image');
-    t.ok(movies.data.movies[0].medium_cover_image, 'Has cover image');
-    t.ok(movies.data.movies[0].rating, 'Has rating');
-    t.ok(movies.data.movies[0].rt_critics_score, 'Has rt score');
-    */
+  eztv.getPages().then( function(pages) {
+    t.ok(pages, 'Has page list');
   });
 });
