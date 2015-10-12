4ecbf49532a9cc4bdc39247643c6977ccb4c233f
stylesuxx <stylesuxx@gmail.com>
2015-10-12 23:57:50 +0200

    Initial commit of the EZTV class.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 import rp from 'request-promise';
+
+class EZTV {
+  constructor(url = 'http://eztvapi.re/') {
+    this.apiUrl = url;
+
+    this.buildQuery = function(options) {
+      var queryString = '';
+      for(var index in options) {
+        queryString += index + '=' + options[index] + '&'
+      }
+
+      return queryString
+    }
+  }
+
+  getPages() {
+    var url = 'shows/';
+    return rp({url: this.apiUrl + url, json:true});
+  }
+}
+
+export default EZTV;
